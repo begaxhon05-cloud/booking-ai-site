69d3a99b35bae0e139e2c1e05e6223ec7f7b355a
Xhon Bega <begaxhon05@gmail.com>
2026-05-06 13:01:53 +0200

    connect admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { propertyInfo } from "./propertyInfo";
+import AdminDashboard from "./AdminDashboard";
 import "./App.css";
 
 export default function App() {
+
+  if (window.location.pathname === "/admin") {
+    return <AdminDashboard />;
+  }
   const [step, setStep] = useState(1);
   const [sent, setSent] = useState(false);
   const [loading, setLoading] = useState(false);
