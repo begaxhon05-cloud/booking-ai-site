9b8f9484056b48915fbe407e3453474fb4a4cfdb
Xhon Bega <begaxhon05@gmail.com>
2026-05-20 09:04:04 +0200

    upgrade elevenlabs turbo voice

--- api/tts.js
+++ api/tts.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify({
           text: cleanText,
-          model_id: "eleven_multilingual_v2",
+          model_id: "eleven_turbo_v2_5",
           voice_settings: {
             stability: 0.45,
             similarity_boost: 0.8,
